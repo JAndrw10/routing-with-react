eff758d53fad46235f1d1df9c3a766e2581ce9ed
JAndrw10 <Joliver1792@gmail.com>
2021-09-03 17:01:26 -0500

    updated

---
+++ src/components/Films.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from "react-router-dom";
+
+export default function Films() {
+
+    const [films, setFilms] = useState([]);
+
+    useEffect(() => {
+        fetch("https://ghibliapi.herokuapp.com/films") // fetch the data
+            .then(res => res.json()) // convert from json to js
+            .then(films => setFilms(films))// set state
+            .catch(err => console.error(err));
+    }, []);
+    return (
+        <>
+            <h1 className="text-center mb-3">Films</h1>
+
+           <div className="container">
+                <div className="row justify-content-around"> 
+                {films.map(film => (
+                <div key={film.id} className="card my-3" style={{ width: "18rem" }}>
+                    <div className="card-body">
+                        <h5 className="card-title">{film.title}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">{film.director}</h6>
+                        <p className="card-text">{film.description}.</p>
+                        <Link to={`/films/${film.id}`} className="card-link">
+                            <button className="btn btn-sm btn-primary">See More</button>
+                        </Link>
+                    </div>
+                </div>
+            ))}
+                </div>
+           </div>
+        </>
+
+    )
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,5 @@
+import React from  'react'
+
+export default function Home() {
+    return <h2>Welcome to my studio Ghilbli rsources. use the nav to select your source.</h2>
+}
